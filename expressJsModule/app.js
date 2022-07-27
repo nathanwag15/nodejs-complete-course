1aae028112c543279bc3df8422c704049b4c5940
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2022-07-26 23:29:07 -0600

    first commit to express

--- expressJsModule/app.js
+++ expressJsModule/app.js
@@ -1,7 +1,27 @@
 const http = require("http");
 
-console.log(routes.someText);
+const express = require("express");
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+// app.use((req, res, next) => {
+//   console.log("in the middleware!");
+//   next(); // Allows the request to continue to the next middleware in line
+// });
+
+app.use("/", (req, res, next) => {
+  console.log("this always runs");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("add product page");
+  res.send("<h1>The add product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("in another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);

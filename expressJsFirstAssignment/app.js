f418e8c3f373f40ef79e397207ca50055d400560
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2022-07-26 23:40:14 -0600

    first assignment express

---
+++ expressJsFirstAssignment/app.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+const express = require("express");
+
+const app = express();
+
+// app.use((req, res, next) => {
+//   console.log("in the middleware!");
+//   next(); // Allows the request to continue to the next middleware in line
+// });
+
+app.use((req, res, next) => {
+  console.log("Login System Initialize");
+  next();
+});
+
+app.use("/users", (req, res, next) => {
+  console.log("list of users");
+  res.send(
+    "<ul><li>User 1</li><li>User 2</li><li>User 3</li><li>User 4</li></ul>"
+  );
+});
+
+app.use((req, res, next) => {
+  console.log("Front Page");
+  res.send("<h2>This is my Login App</h2>");
+});
+
+app.listen(3000);

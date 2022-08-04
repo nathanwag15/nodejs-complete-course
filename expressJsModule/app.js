292d151c9d088a56541bfed5131828f8e6dd46d3
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2022-08-03 23:11:27 -0600

    ejs integration

--- expressJsModule/app.js
+++ expressJsModule/app.js
@@ -1,21 +1,21 @@
 const http = require("http");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -28,7 +28,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "404" });
+  res.status(404).render("404", { pageTitle: "404" });
 });
 
 app.listen(3000);

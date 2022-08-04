25141ab35fd8a90e4835c546d1ce0f5fd376c019
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2022-08-03 22:57:14 -0600

    handlebars integration

--- expressJsModule/app.js
+++ expressJsModule/app.js
@@ -1,12 +1,21 @@
 const http = require("http");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
--- expressJsModule/routes/admin.js
+++ expressJsModule/routes/admin.js
@@ -11,11 +11,14 @@ router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
   });
 });
 
 router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title, docTitle: "Shop", path: "/" });
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 

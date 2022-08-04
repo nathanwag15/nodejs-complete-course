743ad05c8ece95ac8b67d6472160e88579945c49
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2022-08-03 22:25:39 -0600

    final pug

--- expressJsModule/routes/admin.js
+++ expressJsModule/routes/admin.js
@@ -8,11 +8,14 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "admin/add-product",
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({ title: req.body.title, docTitle: "Shop", path: "/" });
   res.redirect("/");
 });
 

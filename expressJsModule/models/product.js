b50104b606a9dc7d452934375954ecfe627e644f
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2022-09-10 13:37:08 -0600

    storing data in files via the model

--- expressJsModule/models/product.js
+++ expressJsModule/models/product.js
@@ -1,15 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  static fetchAll() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
+  }
+};
